a7991399062c36891f74fda09e377ba25857b47a
Omar Abdelhafith <o.arrabi@me.com>
2017-08-07 15:45:13 +0100

    Adding tooltip on button

--- app/src/components/SettingsButtonsComponent.tsx
+++ app/src/components/SettingsButtonsComponent.tsx
@@ -1,16 +1,23 @@
 import React = require('react')
-import { BottomButton } from '../elements/elements'
+import { findDOMNode } from 'react-dom'
+import { BottomButton, ButtonsHolder } from '../elements/elements'
 import { monitorChangeHack } from '../services/KeyboardHacks'
+import ReactTooltip = require('react-tooltip')
 
 interface ISettingsButtonsComponentProps {
 }
 
 export default class SettingsButtonsComponent extends React.Component<ISettingsButtonsComponentProps, any> {
   render () {
-    return <BottomButton
-      onClick={() => {
-        monitorChangeHack()
-      }}
-    />
+    return <ButtonsHolder>
+      <BottomButton
+        data-tip='Cycle Monitor Types'
+        onClick={() => {
+          ReactTooltip.hide(findDOMNode(this.refs.foo))
+          monitorChangeHack()
+        }}
+      />
+      <ReactTooltip effect='solid' type='info' delayShow={700}/>
+    </ButtonsHolder>
   }
 }
